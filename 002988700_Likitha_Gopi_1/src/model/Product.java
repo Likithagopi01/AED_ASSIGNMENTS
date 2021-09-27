3d9324ec75b39269a5a8bbf3b56555646b06b5c2
Likithagopi01 <gopi.l@northeastern.edu>
2021-09-26 21:24:25 -0400

    final commit

---
+++ 002988700_Likitha_Gopi_1/src/model/Product.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Likitha G
+ */
+public class Product {
+    private String fname;
+    private String lname;
+    private String add1;
+    private String DOB;
+    private long phone;
+    private long FAX;
+    private String Email;
+    private int SSN;
+    private long Medical;
+    private long Health;
+    private long Bank;
+    private String certificate;
+    private String Vehicle;
+    private String device;
+    private String Linkedin;
+    private String Internet;
+    private String Photo;
+    private String unique;
+
+    public String getFName() {
+        return fname;
+    }
+
+    public void setFName(String name) {
+        this.fname = name;
+    }
+   public String getLName() {
+        return lname;
+    }
+
+    public void setLName(String name) {
+        this.lname = name;
+    }
+
+    public String getAdd1() {
+        return add1;
+    }
+
+    public void setAdd1(String add1) {
+        this.add1 = add1;
+    }
+
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public long getFAX() {
+        return FAX;
+    }
+
+    public void setFAX(long FAX) {
+        this.FAX = FAX;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public long getMedical() {
+        return Medical;
+    }
+
+    public void setMedical(long Medical) {
+        this.Medical = Medical;
+    }
+
+    public long getHealth() {
+        return Health;
+    }
+
+    public void setHealth(long Health) {
+        this.Health = Health;
+    }
+
+    public long getBank() {
+        return Bank;
+    }
+
+    public void setBank(long Bank) {
+        this.Bank = Bank;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public String getVehicle() {
+        return Vehicle;
+    }
+
+    public void setVehicle(String Vehicle) {
+        this.Vehicle = Vehicle;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getLinkedin() {
+        return Linkedin;
+    }
+
+    public void setLinkedin(String Linkedin) {
+        this.Linkedin = Linkedin;
+    }
+
+    public String getInternet() {
+        return Internet;
+    }
+
+    public void setInternet(String Internet) {
+        this.Internet = Internet;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String Photo) {
+        this.Photo = Photo;
+    }
+
+    public String getUnique() {
+        return unique;
+    }
+
+    public void setUnique(String unique) {
+        this.unique = unique;
+    }
+
+    
+
+    }
+        
+
